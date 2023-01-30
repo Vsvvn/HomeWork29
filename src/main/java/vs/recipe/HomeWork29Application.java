fc523f059031188313e3b1f4cf34c6fcec1619c4
Vsvvn <Vladsun@yandex.ru>
2023-01-30 10:41:00 +0300

    Первый коммит.

---
+++ src/main/java/vs/recipe/HomeWork29Application.java
@@ -0,0 +1,13 @@
+package vs.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork29Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork29Application.class, args);
+    }
+
+}
